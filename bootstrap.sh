ddc53ec0910c3c0a7547a0dc6a36b96981dadc7e
superherointj <5861043+superherointj@users.noreply.github.com>
2021-03-30 12:41:54 -0300

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,8 +12,7 @@ export TF_LOG=TRACE; export TF_LOG_PATH=tf-timeout-demo.log
 
 TF_LOG_PATH=tf-init.log terraform init -backend-config=backend.hcl
 
-TF_LOG_PATH=tf-lke_cluster.log terraform apply -auto-approve -target linode_lke_cluster.
-timeout_demo_lke
+TF_LOG_PATH=tf-lke_cluster.log terraform apply -auto-approve -target linode_lke_cluster.timeout_demo_lke
 
 TF_LOG_PATH=tf-kubeconfig.log terraform output kubeconfig | sed -e 's/^"//' -e 's/"$//' | base64 -d > kubeconfig.yaml
 
